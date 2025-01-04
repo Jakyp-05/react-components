006f24be6d1ce97b06f6d25689e8ad8be8fb1951
Jakyp-05 <jakypjumagulov080gmail.com>
2025-01-04 12:11:48 +0600

    icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,53 @@
-import Button from "./components/ui/button/button";
 import ButtonPr from "./components/ui/buttonPractice";
+import { CallIcon } from "./components/ui/icon/callIcon";
+import { WishlistIcon } from "./components/ui/icon/wishlistIcon";
 
 function App() {
   return (
     <div>
-      <Button variant="success" size="sm">
-        Success Button
-      </Button>
-      <Button variant="danger" size="md">
-        Danger Button
-      </Button>
-      <Button variant="warning" size="lg">
-        Warning Button
-      </Button>
-      <Button variant="primary">Primary Button</Button>
-      <ButtonPr size="sm" variant="second">
-        Вход
-      </ButtonPr>
-      <ButtonPr size="md" variant="global">
-        регистрация
-      </ButtonPr>
-      <ButtonPr variant="global" size="md" onClick={() => alert("Заказать")}>
-        Заказать
-      </ButtonPr>
-      <ButtonPr
-        variant="global"
-        size="xl"
-        onClick={() => alert("Зарегистрироваться")}
+      <div>
+        <ButtonPr size="sm" variant="second">
+          Вход
+        </ButtonPr>
+        <ButtonPr size="md" variant="global">
+          регистрация
+        </ButtonPr>
+        <ButtonPr variant="global" size="md" onClick={() => alert("Заказать")}>
+          Заказать
+        </ButtonPr>
+        <ButtonPr
+          variant="global"
+          size="xl"
+          onClick={() => alert("Зарегистрироваться")}
+        >
+          Зарегистрироваться
+        </ButtonPr>
+        <ButtonPr
+          variant="second"
+          size="xl"
+          onClick={() => alert("Узнать больше")}
+        >
+          Узнать больше
+        </ButtonPr>
+        <ButtonPr
+          variant="global"
+          size="2xl"
+          onClick={() => alert("Попробовать Бесплатно")}
+        >
+          Попробовать Бесплатно
+        </ButtonPr>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          columnGap: "20px",
+        }}
       >
-        Зарегистрироваться
-      </ButtonPr>
-      <ButtonPr
-        variant="second"
-        size="xl"
-        onClick={() => alert("Узнать больше")}
-      >
-        Узнать больше
-      </ButtonPr>
-      <ButtonPr
-        variant="global"
-        size="2xl"
-        onClick={() => alert("Попробовать Бесплатно")}
-      >
-        Попробовать Бесплатно
-      </ButtonPr>
+        <WishlistIcon />
+        <CallIcon />
+      </div>
     </div>
   );
 }
